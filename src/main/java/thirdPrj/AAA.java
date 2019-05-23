be011426ead98f8353a75a7680bfb157e8aada5e
marry <marry@qq.com>
2019-05-23 17:06:46 +0800

    x

--- src/main/java/thirdPrj/AAA.java
+++ src/main/java/thirdPrj/AAA.java
@@ -2,12 +2,6 @@ package thirdPrj;
 
 public class AAA {
 	
-
-	//scott
-	//11111111111111111
-
-	//tiger//scott
-     
-	//branch 'master' of https://github.com/18130090117/third.git
+//Homework
 
 }
